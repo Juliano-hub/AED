c69b34cf9aaf0ab993e1d0605473ac9c40e09de1
Juliano-hub <57720819+Juliano-hub@users.noreply.github.com>
2020-10-26 12:50:37 -0300

    Add files via upload

---
+++ semana 3-7.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int *apaga(int *vetor,int *n);
+
+int main(){
+    int *vetor,tamanho=0,i=0;
+    printf("Digite o valor de n:");
+    scanf("%d",&tamanho);
+
+    vetor = (int*) malloc(sizeof(int)*(tamanho+1) );
+
+    for(i=0;i<tamanho;i++){
+        scanf("%d", (vetor+i) );
+    }
+
+    vetor = apaga(vetor,&tamanho);
+
+    for(i=0;i<tamanho;i++){
+        printf("%d ", *(vetor+i) );
+    }
+
+    free(vetor);
+    return 0;
+}
+
+int *apaga(int *vetor,int *n){
+    int i=0,j=0,aux=0;
+
+    for(i=0; i<*n ;i++){
+        j = i+1;
+
+        while(j < *n){
+            if( *(vetor+i) == *(vetor+j) ){
+                aux = j;
+
+                while(aux < (*n) ){
+                    *(vetor+aux) = *(vetor+(aux+1) );
+                    aux = aux + 1;
+                }
+                *n = *n - 1;
+                
+            }else{
+                j++;
+            }
+
+        }
+
+    }
+
+    vetor = realloc(vetor, sizeof(int)*( (*n) + 1) );
+
+    return (int*) vetor;
+}
